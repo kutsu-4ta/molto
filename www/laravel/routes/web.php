706903a8228fb86bd874466aee966bb5f342d1e6
yamashita <yamashita.antapp@gmail.com>
2023-04-27 01:38:11 +0900

    ✨feature: ルーティング通るのを確認

--- www/laravel/routes/web.php
+++ www/laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Home\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// ホーム画面
+Route::get('/home', [HomeController::class, 'index']);
