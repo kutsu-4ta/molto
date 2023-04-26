e6c75113234d78f2f2d7cef424f679e4129b97e6
yamashita <yamashita.antapp@gmail.com>
2023-04-27 02:58:19 +0900

    ✨feature: react-routerインストール

--- www/react-molto/src/App.tsx
+++ www/react-molto/src/App.tsx
@@ -1,20 +1,46 @@
-import React from 'react';
+import React from "react";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 
 const App: React.FunctionComponent = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <React.StrictMode>
+          <BrowserRouter>
+              <div className="App">
+                  <header className="App-header">
+                  </header>
+                  <h1>Hello React Router</h1>
+              </div>
+              <Routes>
+                  <Route path="/" element={<Index/>}/>
+              </Routes>
+              <Routes>
+                  <Route path="/login" element={<Login/>}/>
+              </Routes>
+              <Routes>
+                  <Route path="/home" element={<Home/>}/>
+              </Routes>
+              <Routes>
+                  <Route path="/myPage" element={<MyPage/>}/>
+              </Routes>
+          </BrowserRouter>
+      </React.StrictMode>
   );
+}
+
+const Login: React.FunctionComponent = () => {
+    return <h2>Login</h2>;
+}
+
+const Index: React.FunctionComponent = () => {
+    return <h2>Index</h2>;
+}
+
+const Home: React.FunctionComponent = () => {
+    return <h2>Home</h2>;
+}
+
+const MyPage: React.FunctionComponent = () => {
+    return <h2>MyPage</h2>;
 }
 
 export default App;
