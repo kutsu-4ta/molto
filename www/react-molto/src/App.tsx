9c61dab9b96c2ce4eb18e21a041eac04b2ac8594
yamashita <yamashita.antapp@gmail.com>
2023-04-29 11:38:11 +0900

    🔧fix: パンくずの実装は優先度下げた

--- www/react-molto/src/App.tsx
+++ www/react-molto/src/App.tsx
@@ -3,7 +3,10 @@ import { Route, BrowserRouter, Routes } from "react-router-dom";
 // アイコン
 import {HomeOutlined, LoginOutlined, SearchOutlined, TeamOutlined, UserOutlined} from '@ant-design/icons';
 // antDesignコンポーネント
-import {Breadcrumb, Layout, Menu, type MenuProps, theme } from "antd";
+import {
+    // Breadcrumb,
+    Layout, Menu, type MenuProps, theme
+} from "antd";
 // ページ群
 import Login from "./views/login/_login";
 import Home from "./views/home/_home";
@@ -34,22 +37,23 @@ const App: React.FunctionComponent = () => {
                     <Layout className="site-layout">
                         <Header style={{padding: 0, background: colorBgContainer}}/>
                         <Content style={{margin: '0 16px'}}>
+                            {/* TODO: パンくず未実装
                             <Breadcrumb style={{margin: '16px 0'}}>
                                 <Breadcrumb.Item>User</Breadcrumb.Item>
                                 <Breadcrumb.Item>Bill</Breadcrumb.Item>
                             </Breadcrumb>
+                            */}
                             <div style={{padding: 24, minHeight: 360, background: colorBgContainer}}>
-                                Bill is a cat.
+                                <Routes>
+                                    <Route path="/login" element={<Login/>}/>
+                                </Routes>
+                                <Routes>
+                                    <Route path="/home" element={<Home/>}/>
+                                </Routes>
+                                <Routes>
+                                    <Route path="/myPage" element={<MyPage/>}/>
+                                </Routes>
                             </div>
-                            <Routes>
-                                <Route path="/login" element={<Login/>}/>
-                            </Routes>
-                            <Routes>
-                                <Route path="/home" element={<Home/>}/>
-                            </Routes>
-                            <Routes>
-                                <Route path="/myPage" element={<MyPage/>}/>
-                            </Routes>
                         </Content>
                         <Footer style={{textAlign: 'center'}}>Ant Design ©2023 Created by Ant UED</Footer>
                     </Layout>
@@ -77,17 +81,17 @@ const _generateMenuItem = (label: React.ReactNode, key: React.Key, icon?: React.
 };
 // サイドメニューの項目
 const items: MenuItem[] = [
-    _generateMenuItem('Home', '1', <HomeOutlined />),
-    _generateMenuItem('Search', '2', <SearchOutlined />),
+    _generateMenuItem(<a href={'/home'}>home</a>, '1', <HomeOutlined />),
+    _generateMenuItem(<a href={'/search'}>search</a>, '2', <SearchOutlined />),
     _generateMenuItem('Libraries', 'sub2', <TeamOutlined />, [
-        _generateMenuItem('Music', '3'),
-        _generateMenuItem('ArtWork', '4')
+        _generateMenuItem(<a href={'/music-works'}>music</a>, '3'),
+        _generateMenuItem(<a href={'/art-works'}>art</a>, '4')
     ]),
     _generateMenuItem('Profile', 'sub1', <UserOutlined />, [
-        _generateMenuItem('Your Profile', '5'),
-        _generateMenuItem('Your Works', '6'),
-        _generateMenuItem('WIP', '8'),
-        _generateMenuItem('Account Settings', '9'),
+        _generateMenuItem(<a href={'/profile'}>profile</a>, '5'),
+        _generateMenuItem(<a href={'/your-works'}>yourWorks</a>, '6'),
+        _generateMenuItem(<a href={'/wip'}>WIP</a>, '8'),
+        _generateMenuItem(<a href={'/account-setting'}>account Setting</a>, '9'),
     ]),
-    _generateMenuItem('Logout', '10', <LoginOutlined />),
+    _generateMenuItem(<a href={'/login'}>login</a>, '10', <LoginOutlined/>),
 ];
