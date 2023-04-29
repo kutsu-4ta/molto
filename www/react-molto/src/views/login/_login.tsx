84f75e1a45218ce8d70aac0f2f850e7b487965ec
yamashita <yamashita.antapp@gmail.com>
2023-04-30 04:47:47 +0900

    ✨feature: ログイン画面_画面のみ作成

--- www/react-molto/src/views/login/_login.tsx
+++ www/react-molto/src/views/login/_login.tsx
@@ -1,53 +1,61 @@
 import React from 'react';
-import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Button, Checkbox, Form, Input } from 'antd';
+import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
+import { Button, Col, Input, Row, Divider } from 'antd';
 
-const App: React.FC = () => {
-    const onFinish = (values: any) => {
-        console.log('Received values of form: ', values);
-    };
+const Login: React.FC = () => {
+    const [passwordVisible, setPasswordVisible] = React.useState(false);
 
     return (
-        <Form
-            name="normal_login"
-            className="login-form"
-            initialValues={{ remember: true }}
-            onFinish={onFinish}
-        >
-            <Form.Item
-                name="username"
-                rules={[{ required: true, message: 'Please input your Username!' }]}
-            >
-                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-            </Form.Item>
-            <Form.Item
-                name="password"
-                rules={[{ required: true, message: 'Please input your Password!' }]}
-            >
-                <Input
-                    prefix={<LockOutlined className="site-form-item-icon" />}
-                    type="password"
-                    placeholder="Password"
-                />
-            </Form.Item>
-            <Form.Item>
-                <Form.Item name="remember" valuePropName="checked" noStyle>
-                    <Checkbox>Remember me</Checkbox>
-                </Form.Item>
-
-                <a className="login-form-forgot" href="">
-                    Forgot password
-                </a>
-            </Form.Item>
-
-            <Form.Item>
-                <Button type="primary" htmlType="submit" className="login-form-button">
-                    Log in
-                </Button>
-                Or <a href="">register now!</a>
-            </Form.Item>
-        </Form>
+        <div className="container" style={{color: "#d7d7d7", paddingLeft: 30}}>
+            <section className="featured">
+                <h2>SIGN UP</h2>
+                <div className="sign-up">
+                    <Row>
+                        <Col span={3} offset={6} style={{marginRight:0}}>
+                            <p>UserId</p>
+                        </Col>
+                        <Col span={9} offset={6} style={{marginLeft:0}}>
+                            <Input required placeholder="moltoUser"/>
+                        </Col>
+                    </Row>
+                    {/* // TODO: プロトタイプでは電話番号認証はしない
+                    <Row>
+                        <Col span={3} offset={6} style={{marginRight:0}}>
+                            <p>phoneNumber</p>
+                        </Col>
+                        <Col span={9} offset={6} style={{marginLeft:0}}>
+                            <Input required placeholder="090-1234-5678" />
+                        </Col>
+                    </Row>
+                    */}
+                    <Row>
+                        <Col span={3} offset={6} style={{marginRight:0}}>
+                            <p>PassWord</p>
+                        </Col>
+                        <Col span={9} offset={6} style={{marginLeft:0}}>
+                            <Input.Password required placeholder="inputStrongPassWord" iconRender={(visible) => (visible ? <EyeTwoTone/> : <EyeInvisibleOutlined/>)}/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={3} offset={6} style={{marginRight:0}}>
+                            <p>PassWord (ReInput)</p>
+                        </Col>
+                        <Col span={9} offset={6} style={{marginLeft:0}}>
+                            <Input.Password required placeholder="inputStrongPassWord" visibilityToggle={{visible: passwordVisible, onVisibleChange: setPasswordVisible}}/>
+                        </Col>
+                    </Row>
+                    <Divider/>
+                    <Row>
+                        <Col span={3} offset={6} style={{marginRight:0}}>
+                        </Col>
+                        <Col span={9} offset={6} style={{marginLeft:0}}>
+                            <Button style={{width: 80}} type="primary">signUp</Button>
+                        </Col>
+                    </Row>
+                </div>
+            </section>
+        </div>
     );
 };
 
-export default App;
+export default Login;
